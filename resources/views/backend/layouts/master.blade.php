ca063f60a3bf15d799acafc8969d465b0ab31c43
shahriar201 <shahriarbrine201@gmail.com>
2022-08-06 12:34:30 +0600

    product create page realtime image

--- resources/views/backend/layouts/master.blade.php
+++ resources/views/backend/layouts/master.blade.php
@@ -100,6 +100,19 @@
 
   </script>
 
+  {{-- Realtime image using Javascript --}}
+  <script type="text/javascript">
+    $(document).ready(function() {
+        $('#image').change(function(e) {
+            var reader = new FileReader();
+            reader.onload = function(e) {
+                $('#showImage').attr('src', e.target.result);
+            }
+            reader.readAsDataURL(e.target.files['0']);
+        });
+    });
+</script>
+
   {{-- @include('backend.partials.status') --}}
 
   @yield('content')
